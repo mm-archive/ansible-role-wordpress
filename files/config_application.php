c4ff106744ed4708bde25cd6a23af02653e45598
Matthew Miller <matthew@mi11er.net>
2014-11-08 00:49:10 -0500

    fix

--- files/config_application.php
+++ files/config_application.php
@@ -1,6 +1,6 @@
 <?php
 $root_dir = dirname(__DIR__);
-$webroot_dir = $root_dir . '/web';
+$webroot_dir = $root_dir . '/public';
 
 /**
  * Use Dotenv to set required environment variables and load .env file in root
